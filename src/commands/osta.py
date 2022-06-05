6c372a2eb1b148446af0f32339620c1a1076be8e
Atte Jokinen <atte.jokinen@tuni.fi>
2022-06-05 20:15:11 +0300

    tehty osta komentoa

--- src/commands/osta.py
+++ src/commands/osta.py
@@ -10,5 +10,11 @@ async def osta_callback(update: Update, context: CallbackContext.DEFAULT_TYPE) -
     msg = (
         "placeholder"
     )
+    #autentikoi
+
+    #näytä grid
+    grid = telegram.reply_keyboard_markup([["1 €","1.5 €","2 €"],["2.5 €","3 €","Muu summa"]],one_time_keyboard = True, resize_keyboard = True)
+    update.message.reply_text(msg, reply_markup = grid)
+    #kirjaa kantaan
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
