5b26f6c1d28001c0d7a074117db768ea5988d1fe
Atte Jokinen <atte.jokinen@tuni.fi>
2022-06-05 17:31:45 +0300

    database connection done

--- src/database.py
+++ src/database.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import pymongo
 import envreader
-import datetime
 
 client = MongoClient(envreader.get_var("MONGO_DB_URI"))
 
@@ -9,46 +8,59 @@ db = client["users"]
 
 payment_schema = {
     'date': {
-        type: type(datetime.date()),
-        required: True
+        "type": str,
+        "required": True
     },
     'amount': {
-        type: int,
-        required: True
+        "type": str,
+        "required": True
     }
 }
 
 user_schema = {
     'handle': {
-        type: str,
+        "type": str,
         "required": True
     },
     'tg_id': {
-        type: str,
+        "type": str,
         "required": True
     },
     'balance': {
-        type: int,
+        "type": int,
         "required": True
     },
     'payments':{
-        type: payment_schema
+        "type": payment_schema
     }
 }
 
-
-
 def new_user(user_id):
-    print("todo")
+    user = {
+        'handle': "TODO",
+        "tg_id": user_id,
+        "balance": 0,
+        "payments": []
+    }
+    return db.users.insert_one(user)
 
 def find_user(user_id):
-    return db.users.find_one({"user_id":user_id})
+    return db.users.find_one({"tg_id":user_id})
 
-def save_payment(maksu):
-    print("todo")
+def new_payment(user_id, maksu):
+    user = find_user(user_id)
+    payment_obj = {
+        "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "amount": maksu
+    }
+    user["payments"].append(payment_obj)
+    user["balance"] += maksu
+    db.users.find_one_and_update({"tg_id":user_id}, {"$set": user})
 
 def get_users():
-    return db.users.find()
+    return list(db.users.find())
 
 def new_purchase(user_id, amount):
-    print("todo")
+    user = find_user(user_id)
+    user["balance"] += amount   
+    return db.users.update_one({"tg_id":user_id}, {"$set": user})
