1d96f9590695c545ef0710462d9b37a5f7e4ee7f
Ilari <raisanen.ilari@outlook.com>
2022-06-05 11:48:50 +0300

    envreader added to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from tokenize import Token
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
 
@@ -13,7 +14,8 @@ async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    TOKEN = envreader.get_var('TOKEN')
+    application = ApplicationBuilder().token(TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
