eb5a7f148bcfd3a065ab9e80f2405fc1e507ed8d
Atte Jokinen <atte.jokinen@tuni.fi>
2022-06-08 15:22:18 +0300

    it works wow

--- src/commands/osta.py
+++ src/commands/osta.py
@@ -1,7 +1,8 @@
 import logging
-from telegram import (Update, ReplyKeyboardMarkup)
+from telegram import (Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, CallbackQuery)
 from telegram.ext import (
     ContextTypes,
+    CallbackQueryHandler
 )
 import auth
 import database as db
@@ -11,12 +12,20 @@ async def osta_callback(update: Update, context) -> None:
         msg = "Joko olet väärässä paikassa tai et ole ottanut bottia käyttöön oikein 🕶️"
     else:
         amount = update.message.text.split(" ")
-        print(amount)
+
+#       summat = [["1","1.5","2"],["2.5","3","Muu summa"]]
+#        napit1 = []
+#        napit2 = []
+#        for i in summat[0]:
+#             napit1.append(InlineKeyboardButton(i,callback_data=i[0]))
+#        for i in summat[1]:
+##            napit2.append(InlineKeyboardButton(i,callback_data=i[0]))
+##        napit = [napit1,napit2]
+#        reply_markup = ReplyKeyboardMarkup(napit)
+#        Bot.send_message(text = "Valitse summa:", reply_markup=reply_markup)
         #näytä rivi nappeja
-        kb = ReplyKeyboardMarkup([["1","1.5","2"],["2.5","3","Muu summa"]], one_time_keyboard=True, resize_keyboard=True)
-        asd = await update.message.reply_text("Valitse piikille lisättävä summa", reply_markup=kb)
-        print("ASDASD")
-        print(asd.text)
+        kb = InlineKeyboardMarkup(inline_keyboard= [[InlineKeyboardButton(text = "FFF", callback_data = (1, 1)), InlineKeyboardButton(text = "AAA", callback_data = (2, 2))]], one_time_keyboard=True, resize_keyboard=True)
+        await update.message.reply_text(text="123123" , reply_markup=kb)
         #db.new_purchase(update.effective_user.id, amount)
         msg = "Piikki lisätty. Piikkisi nyt on %s €" % (10000000)
-    await update.message.reply_text(msg)
+    #await update.message.reply_text(msg)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import logging
 from tokenize import Token
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
+from telegram.ext import Updater, ApplicationBuilder, CallbackContext, CommandHandler, CallbackQueryHandler, ContextTypes
 
 import envreader
 
@@ -14,15 +14,44 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+def kb_callback(update, context):
+    print("jojoasjdaosjdoasd")
+    query = update.callback_query
+    print(query.data)
+    query.answer()
+
+async def list_button(update: Update, context: ContextTypes) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    print("AAAAAAAAA")
+    query = update.callback_query
+    await query.answer()
+    # Get the data from the callback_data.
+    # If you're using a type checker like MyPy, you'll have to use typing.cast
+    # to make the checker get the expected type of the callback_data
+    #number, number_list = cast(Tuple[int, List[int]], query.data)
+    # append the number to the list
+    #number_list.append(number)
+
+    #await query.edit_message_text(
+    #    text=f"So far you've selected {number_list}. Choose the next item:",
+    #    reply_markup=build_keyboard(number_list),
+    #)
+    print(query)
+    # we can delete the data stored for the query, because we've replaced the buttons
+    context.drop_callback_data(query)
+
 if __name__ == '__main__':
     TOKEN = envreader.get_var('TOKEN')
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TOKEN).arbitrary_callback_data(True).build()
     
     handlers = [CommandHandler('moro', moro_callback),
                 CommandHandler('osta', osta_callback),
-                CommandHandler('maksa', maksa_callback)]
+                CommandHandler('maksa', maksa_callback)
+                ]
     
     for handler in handlers:
         application.add_handler(handler)
+    #update = Update(1, use_context = True)
+    application.add_handler(CallbackQueryHandler(list_button))
     
     application.run_polling()
