5c017407b037bf0b2f6bc7f977129fca2ca86394
Atte Jokinen <atte.jokinen@tuni.fi>
2022-06-10 22:52:08 +0300

    maksut toimii nyt

--- src/commands/osta.py
+++ src/commands/osta.py
@@ -1,31 +1,39 @@
 import logging
-from telegram import (Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, CallbackQuery)
+from telegram import (Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, CallbackQuery, )
 from telegram.ext import (
     ContextTypes,
-    CallbackQueryHandler
+    CallbackQueryHandler,
+    
 )
 import auth
 import database as db
 
-async def osta_callback(update: Update, context) -> None:
-    if(auth.authenticate_user(update.effective_user.id) == False):
-        msg = "Joko olet väärässä paikassa tai et ole ottanut bottia käyttöön oikein 🕶️"
-    else:
-        amount = update.message.text.split(" ")
+def is_float(element) -> bool:
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
 
-#       summat = [["1","1.5","2"],["2.5","3","Muu summa"]]
-#        napit1 = []
-#        napit2 = []
-#        for i in summat[0]:
-#             napit1.append(InlineKeyboardButton(i,callback_data=i[0]))
-#        for i in summat[1]:
-##            napit2.append(InlineKeyboardButton(i,callback_data=i[0]))
-##        napit = [napit1,napit2]
-#        reply_markup = ReplyKeyboardMarkup(napit)
-#        Bot.send_message(text = "Valitse summa:", reply_markup=reply_markup)
-        #näytä rivi nappeja
-        kb = InlineKeyboardMarkup(inline_keyboard= [[InlineKeyboardButton(text = "FFF", callback_data = (1, 1)), InlineKeyboardButton(text = "AAA", callback_data = (2, 2))]], one_time_keyboard=True, resize_keyboard=True)
-        await update.message.reply_text(text="123123" , reply_markup=kb)
-        #db.new_purchase(update.effective_user.id, amount)
-        msg = "Piikki lisätty. Piikkisi nyt on %s €" % (10000000)
-    #await update.message.reply_text(msg)
+async def osta_callback(update: Update, context) -> None:
+    msg = update.message.text.split(" ")
+    amounts = [1, 1.5, 2, 2.5, 3]
+    if(auth.authenticate_user(update.effective_user.id) == False):
+        await update.message.reply_text("Joko olet väärässä paikassa tai et ole ottanut bottia käyttöön oikein 🕶️")
+        return
+
+    if(len(msg)==1): #jos annettu vain /osta, näytetään valikko mistä valitaan summa
+        kb = ReplyKeyboardMarkup.from_column([KeyboardButton('/osta {} €'.format(i)) for i in amounts], one_time_keyboard=True)
+        await update.message.reply_text(text="Valitse summa. Jos haluat muun summan, käytä /osta <summa>. esim /osta 1" , reply_markup=kb)
+    elif(len(msg)>1): #Jos annettu myös summa lisätään summa piikkiin
+        amount = msg[1]
+        #if(is_int(amount) | is_float(amount)): #tarkista että summa on sopiva numero
+        if(is_float(amount)):
+            #Add the amount to the database for user
+            user_id = update.effective_user.id
+            user = db.find_user(user_id)
+            if(user != None):
+                db.new_purchase(user["tg_id"], -1 * float(amount))
+                await update.message.reply_text("Tili done")
+        else: #jos ei ole hauku käyttäjää
+            await update.message.reply_text("Tarkista komento. Esimerkki: /osta 2")
--- src/main.py
+++ src/main.py
@@ -14,32 +14,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-def kb_callback(update, context):
-    print("jojoasjdaosjdoasd")
-    query = update.callback_query
-    print(query.data)
-    query.answer()
-
-async def list_button(update: Update, context: ContextTypes) -> None:
-    """Parses the CallbackQuery and updates the message text."""
-    print("AAAAAAAAA")
-    query = update.callback_query
-    await query.answer()
-    # Get the data from the callback_data.
-    # If you're using a type checker like MyPy, you'll have to use typing.cast
-    # to make the checker get the expected type of the callback_data
-    #number, number_list = cast(Tuple[int, List[int]], query.data)
-    # append the number to the list
-    #number_list.append(number)
-
-    #await query.edit_message_text(
-    #    text=f"So far you've selected {number_list}. Choose the next item:",
-    #    reply_markup=build_keyboard(number_list),
-    #)
-    print(query)
-    # we can delete the data stored for the query, because we've replaced the buttons
-    context.drop_callback_data(query)
-
 if __name__ == '__main__':
     TOKEN = envreader.get_var('TOKEN')
     application = ApplicationBuilder().token(TOKEN).arbitrary_callback_data(True).build()
@@ -51,7 +25,5 @@ if __name__ == '__main__':
     
     for handler in handlers:
         application.add_handler(handler)
-    #update = Update(1, use_context = True)
-    application.add_handler(CallbackQueryHandler(list_button))
     
     application.run_polling()
